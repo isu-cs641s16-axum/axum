5f1276c3b5c23500675866308316c4238eda9db7
David Johnston <dwtj@iastate.edu>
2016-05-01 21:18:53 -0500

    Add `concatenated_schema` def and a related lemma
    This lemma proves that concatenated schema are also schema.

--- PigFormalism.v
+++ PigFormalism.v
@@ -122,6 +122,52 @@ Fixpoint schema_column_is_int (t: ty) (c: col) : bool :=
   end.
 
 
+(* The concatenation of schemas T1 and T2 is T3. *)
+Inductive concatenated_schema: ty -> ty -> ty -> Prop :=
+
+  | CatSchemaNil: forall T,
+      schema_ty TNil ->
+      schema_ty T ->
+      concatenated_schema TNil T T
+
+  | CatSchemaCons: forall T1 THead T1tail T2 T3 T3tail,
+      schema_ty T1 ->
+      schema_ty T2 ->
+      T1 = TCons THead T1tail ->
+      T3 = TCons THead T3tail ->
+      concatenated_schema T1tail T2 T3tail ->
+      concatenated_schema T1 T2 T3.
+
+
+(* The concatenation of schemas T1 and T2 is also a schema. *)
+Lemma concatenation_of_schemas_is_schema : forall T1 T2 T3: ty,
+  schema_ty T1 ->
+  schema_ty T2 ->
+  concatenated_schema T1 T2 T3 ->
+  schema_ty T3.
+Proof.
+  intros T1 T2 T3 H_schema_T1 H_schema_T2 H_concat. induction H_concat.
+  - (* CatSchemaNil *) auto.
+  - (* CatSchemaCons *) rewrite -> H2. inversion H.
+    + (* STNil *) rewrite <- H3 in H1. inversion H1.
+    + (* STConsInt *)
+      (* Concretize stmnt to prove to use TInt & make IHH_concat usable. *)
+      rewrite <- H4 in H1.
+      inversion H1.
+      apply STConsInt.
+      apply IHH_concat.
+      * rewrite H7 in H3. assumption.
+      * assumption.
+    + (* STConsBag *)
+      (* Concretize stmnt to prove to use TBag & make IHH_concat usable. *)
+      rewrite <- H5 in H1.
+      inversion H1.
+      apply STConsBag.
+      * assumption.
+      * apply IHH_concat; subst; try assumption.
+Qed.
+
+
 Definition context := partial_map ty.
 
 (* ################################### *)
