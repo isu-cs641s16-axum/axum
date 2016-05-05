c1313ac88183007ba6450d39776dd82a98db299b
David Johnston <dwtj@iastate.edu>
2016-05-05 07:31:42 -0500

    Draft initial def of `filtered`

--- Filtering.v
+++ Filtering.v
@@ -4,7 +4,16 @@ Require Export Pig.Relations.
 Definition filter (s: schema_ty) : Type := (support s) -> bool.
 
 
+Definition is_filtered (s:schema_ty) (r: relation s) (f:filter s) (r': relation s) :=
+  let m  := (relation_data s r) in
+  let m' := (relation_data s r') in
+  forall tup: (support s),
+    multiplicity m tup = match (f tup) with
+                         | true => multiplicity m' tup
+                         | false => 0
+                         end.
+
 Inductive filtered (s: schema_ty): relation s -> filter s -> relation s -> Prop :=
   | Filtered : forall (f: filter s) (r r': relation s),
-     (* TODO: Add constraints. *)
+     is_filtered s r f r' ->
      filtered s r f r'.
